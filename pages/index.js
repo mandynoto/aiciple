df3bf4c1ee133fc531b7c8eb4d7bd088de58d3c7
Mandy Noto <mandynoto@icloud.com>
2023-06-21 22:16:19 -0700

    feat: improve frontend and be able to sign up

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
 import Head from "next/head";
 import Link from "next/link";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
 
 export default function Home() {
   const { isLoading, error, user } = useUser();
@@ -8,15 +9,46 @@ export default function Home() {
   if (error) return <div>{error.message}</div>;
 
   return (
-    <div>
+    <>
       <Head>
         <title>aiciple - login or signup</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-      <div>
-        {!!user && <Link href="api/auth/logout">Logout</Link>}
-        {!user && <Link href="/api/auth/login">Login</Link>}
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
+        <div>
+          {!!user && <Link href="api/auth/logout">Logout</Link>}
+          {!user && (
+            <>
+              <Link
+                href="/api/auth/login"
+                className="text-whit rounded-md bg-hangin-100 px-4 py-2 hover:bg-hangin-200"
+              >
+                Login
+              </Link>
+              <Link
+                href="/api/auth/signup"
+                className="text-whit rounded-md bg-hangin-100 px-4 py-2 hover:bg-hangin-200"
+              >
+                Sign up
+              </Link>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx.req, ctx.res);
+  if (!!session) {
+    return {
+      redirect: {
+        destination: "/chat",
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hangin: "#3F83D1", // Replace with your desired color value
+        "hangin-100": "#ab9c7f", // Replace with your desired color value
+        "hangin-200": "#958669", // Replace with your desired color value
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
