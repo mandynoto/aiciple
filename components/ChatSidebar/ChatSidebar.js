8e5ee5f098bd9b6133b4b4d0f936225ba3ba25a8
Mandy Noto <89042473+mandynoto@users.noreply.github.com>
2023-06-23 20:37:10 -0700

    Revert "Revert "Feat/talk to aiciple" (#2)"
    This reverts commit 3713acd2d3a0557a9d8e401f83cd5cf27fcf6587.

--- components/ChatSidebar/ChatSidebar.js
+++ components/ChatSidebar/ChatSidebar.js
@@ -1,9 +1,9 @@
-import Link from "next/link";
+import Link from "next/link"
 
 export const ChatSidebar = () => {
   return (
     <div className="sidebar">
       <Link href="/api/auth/logout">Logout</Link>
     </div>
-  );
-};
+  )
+}
