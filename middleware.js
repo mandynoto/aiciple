3713acd2d3a0557a9d8e401f83cd5cf27fcf6587
Mandy Noto <89042473+mandynoto@users.noreply.github.com>
2023-06-23 20:36:20 -0700

    Revert "Feat/talk to aiciple" (#2)

--- middleware.js
+++
@@ -1,7 +0,0 @@
-import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge"
-
-export default withMiddlewareAuthRequired()
-
-export const config = {
-  matcher: ["/api/chat/:path*", "/chat/:path*"],
-}
