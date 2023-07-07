4b2982692dbb1b285f1794edc66ba95ac86e0f58
Mandy Noto <mandynoto@icloud.com>
2023-07-06 18:53:40 -0700

    feat(api) validate adding messages to chat

--- pages/api/chat/addMessageToChat.js
+++ pages/api/chat/addMessageToChat.js
@@ -9,9 +9,42 @@ export default async function handler(req, res) {
     const db = client.db("aiciple")
 
     const { chatId, role, content } = req.body
+
+    let objectId
+    try {
+      objectId = new ObjectId(chatId)
+    } catch (e) {
+      res.status(422).json({
+        message: "Invalid chat ID",
+      })
+      return
+    }
+
+    // Validate content data
+    if (
+      !content ||
+      typeof content !== "string" ||
+      (role === "user" && content.length > 200) ||
+      (role === "assistent" && content.length > 100000)
+    ) {
+      res.status(422).json({
+        message:
+          "content: please write a non-empty message less than 200 characters",
+      })
+      return
+    }
+
+    // Validate role
+    if (role !== "user" && role !== "assistant") {
+      res.satus(422).json({
+        message: "role can only be assistent or user",
+      })
+      return
+    }
+
     const chat = await db.collection("chats").findOneAndUpdate(
       {
-        _id: new ObjectId(chatId),
+        _id: objectId,
         userId: user.sub,
       },
       {
