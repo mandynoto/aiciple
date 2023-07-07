e4e7f7f8268549b57c0085cccd38245630b8da65
Mandy Noto <mandynoto@icloud.com>
2023-07-06 18:27:52 -0700

    feat(api): redirect to new chat on invalid chatid

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -177,6 +177,16 @@ export default function ChatPage({ chatId, title, messages = [] }) {
 export const getServerSideProps = async (ctx) => {
   const chatId = ctx.params?.chatId?.[0] || null
   if (chatId) {
+    let objectId
+    try {
+      objectId = new ObjectId(chatId)
+    } catch (e) {
+      return {
+        redirect: {
+          destination: "/chat",
+        },
+      }
+    }
     const { user } = await getSession(ctx.req, ctx.res)
     const client = await clientPromise
     const db = client.db("aiciple")
